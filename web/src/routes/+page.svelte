915087b8b12b5241e8b22bf90fdc505cb03467e2
faust <me@fau.dev>
2023-08-06 10:38:39 -0400

    initial commit

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="container flex h-screen justify-center items-center">
+    <div class="columns-3xs ...">
+      <h1 class="text-3xl text-center font-bold">
+        logs.fau.dev
+      </h1>
+      <p class="text-center">analytics engine for lost ark parses</p>
+    </div>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
