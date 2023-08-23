f1325b35ea4cec14d80f680fff0bd8812447c219
faust <me@fau.dev>
2023-08-23 07:52:39 -0400

    use env var to specify api server

--- web/src/routes/me/+page.server.ts
+++ web/src/routes/me/+page.server.ts
@@ -1,9 +1,17 @@
 import type {PageServerLoad} from './$types';
+import {env} from '$env/dynamic/private';
 
 export const load: PageServerLoad = async ({cookies}) => {
+    let url = "http"
+    if (env.LBF_API_SERVER_ADDRESS_SECURE == "true") {
+        url += "s"
+    }
+    url += "://" + env.LBF_API_SERVER_ADDRESS
+
     const me = await fetch(
-        "http://localhost:3000/api/users/@me",
-        {headers: { cookie: "sessions=" + cookies.get("sessions")}},
+        url + "/api/users/@me",
+        {headers: {cookie: "sessions=" + cookies.get("sessions")}},
     )
+
     return await me.json();
 };
